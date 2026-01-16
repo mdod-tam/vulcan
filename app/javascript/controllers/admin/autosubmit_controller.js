db18fa8843da35346ae7dfb74300a2cc8706d5e9
Alejandro Diaz Meinecke <admc1337@gmail.com>
2026-01-16 11:38:02 -0500

    reverted index.js

--- app/javascript/controllers/admin/autosubmit_controller.js
+++ app/javascript/controllers/admin/autosubmit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    delay: { type: Number, default: 300 },
+    delay: { type: Number, default: 150 },
   };
 
   connect() {
