ebcde70531a8f012636812e9b176ad3ed54503b2
mepps <mepps1627@gmail.com>
2026-05-13 10:01:33 -0400

    Add if not exists true for dev

--- db/migrate/20260506183855_add_application_transfer_id_to_applications.rb
+++ db/migrate/20260506183855_add_application_transfer_id_to_applications.rb
@@ -1,5 +1,5 @@
 class AddApplicationTransferIdToApplications < ActiveRecord::Migration[8.1]
   def change
-    add_column :applications, :application_transfer_id, :string
+    add_column :applications, :application_transfer_id, :string, if_not_exists: true
   end
 end
