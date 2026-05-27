884ea58e12947027f2e26294917b9f9005e3e9f8
David Bahar <dbahar@gwu.edu>
2026-05-27 16:01:47 -0400

    Backfill user email search tokens

--- db/migrate/20260526123000_create_user_email_search_tokens.rb
+++ db/migrate/20260526123000_create_user_email_search_tokens.rb
@@ -11,5 +11,16 @@ class CreateUserEmailSearchTokens < ActiveRecord::Migration[8.1]
 
     add_index :user_email_search_tokens, :token_digest
     add_index :user_email_search_tokens, %i[user_id token_digest], unique: true
+
+    reversible do |dir|
+      dir.up do
+        User.reset_column_information
+        UserEmailSearchToken.reset_column_information
+
+        say_with_time 'Backfilling user email search tokens' do
+          User.find_each(&:rebuild_email_search_tokens!)
+        end
+      end
+    end
   end
 end
